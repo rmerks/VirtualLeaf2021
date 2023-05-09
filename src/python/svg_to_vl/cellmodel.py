2a2ae03527a80cf9e88b9f1d31fd3329ceb00819
Richard van Nieuwenhoven <richard.nieuwenhoven@tuwien.ac.at>
2023-05-09 23:53:36 +0200

    convert svg to model success

--- src/python/svg_to_vl/cellmodel.py
+++ src/python/svg_to_vl/cellmodel.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import xml.etree.ElementTree as ET
 
 class Mesh:
 
@@ -6,7 +7,9 @@ class Mesh:
         self.walls=list()
         self.nodes=list()
         self.cells=list()
-        self.nodeNr=1
+        self.nodeNr=0
+        self.wallNr=0
+        self.cellNr=0
         self.mul=0.75
         
     def getNode(self,x,y):
@@ -24,12 +27,14 @@ class Mesh:
         for existing_wall in self.walls:
             if existing_wall.isBetween(node1,node2):
                 return existing_wall 
-        wall = Wall(node1,node2)
+        wall = Wall(node1,node2,self.wallNr)
+        self.wallNr+=1
         self.walls.append(wall)
         return wall
     
     def getCell(self):
-        cell = Cell(self)
+        cell = Cell(self,self.cellNr)
+        self.cellNr+=1
         self.cells.append(cell)
         return cell
 
@@ -41,28 +46,71 @@ class Mesh:
         for wall in self.walls:
             if wall.isBorder():
                 cell = self.getCell()
+                cell.setType("#ffffff")
                 cell.defineInnerCell(wall)
-        print("first")
+        another=False;
         for cell in self.cells:
             if cell.retryDefineInnerCell() == None:
-                print(cell)
-        print("next")
+                another=True
+        if another:
+            for cell in self.cells:
+                if cell.retryDefineInnerCell() == None:
+                    print("not successful!")
+        self.removeCircumverence()
+                    
+        
+    def removeCircumverence(self):
+        longest = 0.0
+        self.boundary_polygon = None
         for cell in self.cells:
-            if cell.retryDefineInnerCell() == None:
-                print(cell)
-
+            size=cell.circumverence()
+            if size>longest:
+                longest=size
+                self.boundary_polygon=cell
+        self.cells.remove(self.boundary_polygon)
+        for wall in self.boundary_polygon.getWalls():
+            wall.getCells().remove(self.boundary_polygon)
+            
+    def toXml(self):
+        docLeaf = ET.Element("leaf")
+        docNodes = ET.SubElement(docLeaf, "nodes", n = str(len(self.nodes)))
+        for node in self.nodes:
+            node.toXml(docNodes)
+        docCells = ET.SubElement(docLeaf, "cells" \
+                                 , n = str(len(self.cells)) \
+                                 , magnfication="1" \
+                                 , nchem="0" \
+                                 , offsetx="0" \
+                                 , offsety="0" \
+                                 , base_area="0" \
+                                 )
+        for cell in self.cells:
+            cell.toXml(docCells)
+        self.boundary_polygon.toBoundaryBolygon(docCells)
+        docWalls = ET.SubElement(docLeaf, "walls", n = str(len(self.walls)))
+        for wall in self.walls:
+            wall.toXml(docWalls)
+        return docLeaf
+        
+        
 class Node:
     def __init__(self,x,y,nr):
-        self.id= nr
+        self.nr= nr
         self.x=x
         self.y=y
         self.walls=list()
         
     def distance(self, x,y):
         return sqrt(pow(self.x-x,2) + pow(self.y-y,2))
-        
+
+    def distanceO(self, other):
+        return sqrt(pow(self.x-other.x,2) + pow(self.y-other.y,2))
+            
     def addWall(self, wall):
         self.walls.append(wall)
+        
+    def getNr(self):
+        return self.nr
         
     def getNextBorderNode(self, existing_walls):
         potential_next_borders = [value for value in self.walls if (value not in existing_walls) and value.isBorder()]
@@ -74,16 +122,29 @@ class Node:
                 return wall_to_add.getNode(1)
             return 
         else:
-            return None
-        
+            return None    
+            
+    def toXml(self,docNodes):
+        border = False
+        fixed = False
+        for wall in self.walls:
+            border = border or wall.isBorder();
+            for cell in wall.getCells():
+                fixed = fixed or cell.getType() == "#000000"
+        docNode = ET.SubElement(docNodes, "node", nr=str(self.nr), x = str(self.x), y = str(self.y), sam="false", boundary=str(border).lower(), fixed=str(fixed).lower())
+
 class Wall:
-    def __init__(self, node1, node2):
+    def __init__(self, node1, node2,nr):
+        self.nr = nr
         self.node1 = node1
         self.node2 = node2
         self.node1.addWall(self)
         self.node2.addWall(self)
         self.cells=list()
         
+    def getNr(self):
+        return self.nr
+    
     def isBetween(self, node1, node2):
         return (self.node1 == node1 and self.node2 == node2) or \
               (self.node1 == node2 and self.node2 == node1)
@@ -100,13 +161,57 @@ class Wall:
             return self.node1 
         else: 
             return self.node2
+        
+    def getCells(self):
+        return self.cells  
+    
+    def length(self):
+        return self.node1.distanceO(self.node2)
+    
+    def toXml(self,wallNodes):
+        if len(self.cells) == 1:
+            c2nr=-1
+        else:        
+            c2nr=self.cells[1].getNr()
+        wallNode = ET.SubElement(wallNodes, "wall" \
+                                 , length=str(self.length()) \
+                                 , c1=str(self.cells[0].getNr()) \
+                                 , c2=str(c2nr) \
+                                 , index=str(self.nr) \
+                                 , n1=str(self.node1.getNr()) \
+                                 , n2=str(self.node2.getNr()) \
+                                 , wall_type="normal"  \
+                                 , viz_flux="0" \
+                                 )
+        ET.SubElement(wallNode, "transporters1")
+        ET.SubElement(wallNode, "transporters2")
 
 class Cell:
-    def __init__(self, mesh):
+    def __init__(self, mesh,nr):
+        self.nr=nr
         self.walls = list()
         self.mesh = mesh
         self.firstNode = None
         self.lastNode = None
+        self.type = None
+    
+    def circumverence(self):
+        size = 0.0
+        for wall in self.walls:
+            size += wall.length()
+        return size
+        
+    def setType(self,type):
+        self.type=type
+        
+    def getNr(self):
+        return self.nr
+    
+    def getWalls(self):
+        return self.walls
+        
+    def getType(self):
+        return self.type
         
     def addNode(self, x, y):
         self.addNodeO(self.mesh.getNode(x,y)) 
@@ -140,7 +245,7 @@ class Cell:
         wall.addCell(self)
         self.firstNode = wall.getNode(1)
         self.lastNode = wall.getNode(2)
-        self.retryDefineInnerCell()
+        self.retryDefineInnerCell()    
             
     def retryDefineInnerCell(self):
         if self.lastNode == None:
@@ -164,4 +269,51 @@ class Cell:
             else:
                 return None
         return self
-        
+    
+    def toXml(self,cellNodes):
+        border = 0
+        for wall in self.walls:
+            if wall.isBorder():
+                border = 1
+        cellType = "0"
+        if self.type == "#ffffff":
+            cellType = "1"
+        if self.type == "#0000f8":
+            cellType = "2"
+        if self.type == "#009000":
+            cellType = "3"
+        self.toXmlI("cell",cellNodes,border,cellType,self.nr,self.type == "#000000")
+
+    def toXmlI(self,nodeName,cellNodes,border,cellType,nr,fixed):        
+        cellNode = ET.SubElement(cellNodes, nodeName \
+                                 , boundary=str(border) \
+                                 , cell_type=cellType \
+                                 , target_area="0" \
+                                 , lambda_celllength="0" \
+                                 , at_boundary=str(border==0).lower() \
+                                 , dead="false" \
+                                 , target_length="0" \
+                                 , stiffness="0" \
+                                 , index=str(nr) \
+                                 , source="false" \
+                                 , pin_fixed=str(fixed).lower() \
+                                 , area="0" \
+                                 , fixed=str(fixed).lower() \
+                                 , div_counter="0" \
+                                 )
+        nextNode = self.firstNode
+        ET.SubElement(cellNode, "node" , n=str(nextNode.getNr()))
+        for wall in self.walls:
+            if wall.getNode(1) == nextNode:
+                nextNode=wall.getNode(2)
+            else:
+                nextNode=wall.getNode(1)
+            if  nextNode != self.firstNode:
+                ET.SubElement(cellNode, "node" , n=str(nextNode.getNr()))
+        for wall in self.walls:
+            ET.SubElement(cellNode, "wall" , w=str(wall.getNr()))
+
+    def toBoundaryBolygon(self,cellNodes):
+        self.toXmlI("boundary_polygon",cellNodes,0,"0",-1,False)
+
+
--- src/python/svg_to_vl/readsvg.py
+++ src/python/svg_to_vl/readsvg.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 import re
 import cellmodel
 from math import sqrt
@@ -6,26 +7,9 @@ from math import sqrt
 svgFileName= '/home/ritchie/git/gallpaper/gall_vl'
 namespaces = {'inkscape': 'http://www.inkscape.org/namespaces/inkscape','svg': 'http://www.w3.org/2000/svg'}
 
-def distance(tuple1, tuple2):
-    return sqrt(pow(tuple1[0]-tuple2[0],2)+ pow(tuple1[1]-tuple2[1],2))
-
-def addIfNotExists(existing, tupleToAdd, mul):
-    for current in existing:
-        if distance(current,tupleToAdd) < mul:
-             return current;
-    existing.add(tupleToAdd)
-    return tupleToAdd
-
-def getNodeNear(existing, tupleToAdd, mul,cell):
-    for count, current in enumerate(existing):
-        if distance(current,tupleToAdd) < mul:
-            cell.append(count)
-            return current;
-    raise Exception("No near node found") 
 
 def parseSvg(svgFileName):
     mytree = ET.parse(svgFileName+'.svg')
-  
     return mytree
 
 def parseCoordinates(curr, coord, absolute):
@@ -40,64 +24,33 @@ def getStrokeFromStyle(style):
     style = re.split(';|:', style)
     return style[style.index('stroke')+1]
 
-    
 def getCoordsFromPath(d):
     return  re.split('m |M | z| Z| ', d)
 
-def readNodesFromSvg(svgFileName,mul):
-    mytree=parseSvg(svgFileName)
-    myroot = mytree.getroot()
-    allC = set()
-    curr = [0.0, 0.0]
-    for x in myroot.findall('svg:g',namespaces):
-        for y in x.findall('svg:path',namespaces):
-            strokev=getStrokeFromStyle(y.attrib['style'])
-            d = y.attrib['d']
-            coordiv = getCoordsFromPath(d)
-            reset=[curr[0],curr[1]]
-            for count in range(2,len(coordiv)):
-                curr=parseCoordinates(curr,coordiv[count-1],d.startswith('M') or count == 2);
-                addIfNotExists(allC,tuple(curr),mul);
-            curr=[reset[0],reset[1]]
-    return list(allC)   
 
-def readNodesFromSvgAndReduce(svgFileName, allC, mul,mesh):
+def readNodesFromSvg(svgFileName, mesh):
     curr = [0.0, 0.0]
-    cells = list();
     mytree=parseSvg(svgFileName)
     myroot = mytree.getroot()
     for x in myroot.findall('svg:g',namespaces):
         for y in x.findall('svg:path',namespaces):
             mcell=mesh.getCell()
-            strokev=getStrokeFromStyle(y.attrib['style'])
+            mcell.setType(getStrokeFromStyle(y.attrib['style']))
             d = y.attrib['d']
             coordiv = getCoordsFromPath(d)
-            str1 = 'M ';
             reset=[curr[0],curr[1]]
-            cell=list()
             for count in range(2,len(coordiv)):
                 curr=parseCoordinates(curr,coordiv[count-1],d.startswith('M') or count == 2);
-                xy = getNodeNear(allC,tuple(curr),mul,cell);
-                mcell.addNode(xy[0],xy[1])
-                str1= str1 + str(xy[0]) + ',' + str(xy[1]) + ' '
-            str1= str1 + 'Z'
+                mcell.addNode(curr[0],curr[1])
             mcell.addClosingWall()
-            cells.append(cell)
             curr=[reset[0],reset[1]]
-            y.set('d',str1)
-    mytree.write(svgFileName + '-out.svg')
-    return cells
-    
-mul=0.75
-allC = readNodesFromSvg(svgFileName, mul)
-    
-print(len(allC))  
+
       
 mesh = cellmodel.Mesh() 
-cells = readNodesFromSvgAndReduce(svgFileName, allC, mul,mesh)
-
-print(cells)
-      
+readNodesFromSvg(svgFileName, mesh)
+     
 mesh.defineInnerCells()
 
-print(mesh)
+tree = mesh.toXml()
+pretty=minidom.parseString(ET.tostring(tree, encoding='utf8', method='xml')).toprettyxml(indent = "   ")
+print(pretty)
